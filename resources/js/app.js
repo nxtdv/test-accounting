9ec35c011625d4eda13e642027508bfed522e42d
Ethan <frags31560@gmail.com>
2023-01-17 11:27:45 +0100

    remove unnecessary comments

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,9 +16,6 @@ window.Vue = require('vue').default;
  * Eg. ./components/Loader.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
 Vue.component('home', require('./views/home.vue').default);
 Vue.component('solde', require('./views/solde.vue').default);
 
